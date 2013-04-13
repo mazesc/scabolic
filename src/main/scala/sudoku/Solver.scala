39706ff3b58dee9870a013b697e000920bad0a52
Régis Blanc <regwblanc@gmail.com>
2013-04-13 16:40:35 +0200

    adapt solver to api

--- src/main/scala/sudoku/Solver.scala
+++ src/main/scala/sudoku/Solver.scala
@@ -16,38 +16,18 @@ object Solver {
       yield !vars(r1)(c)(k) || !vars(r2)(c)(k)
     val uniqueInRows = for(r <- 0 to 8; k <- 0 to 8; c1 <- 0 to 7; c2 <- c1+1 to 8)
       yield !vars(r)(c1)(k) || !vars(r)(c2)(k)
-    val uniqueInGrid =
-      (for(k <- 0 to 8; i <- 0 to 2; j <- 0 to 2; r <- 0 to 2; c1 <- 0 to 1; c2 <- c1+1 to 2)
-        yield !vars(3*i + r)(3*j + c1)(k) || !vars(3*i + r)(3*j + c2)(k)) ++
-      (for(k <- 0 to 8; i <- 0 to 2; j <- 0 to 2; r1 <- 0 to 2; c1 <- 0 to 2; c2 <- 0 to 2; r2 <- r1+1 to 2)
-        yield !vars(3*i + r1)(3*j + c1)(k) || !vars(3*i + r2)(3*j + c2)(k))
+    val uniqueInGrid1 =
+      for(k <- 0 to 8; i <- 0 to 2; j <- 0 to 2; r <- 0 to 2; c1 <- 0 to 1; c2 <- c1+1 to 2)
+        yield !vars(3*i + r)(3*j + c1)(k) || !vars(3*i + r)(3*j + c2)(k)
+    val uniqueInGrid2 =
+      for(k <- 0 to 8; i <- 0 to 2; j <- 0 to 2; r1 <- 0 to 2; c1 <- 0 to 2; c2 <- 0 to 2; r2 <- r1+1 to 2)
+        yield !vars(3*i + r1)(3*j + c1)(k) || !vars(3*i + r2)(3*j + c2)(k)
     val forcedEntries = for(r <- 0 to 8; c <- 0 to 8 if sudoku(r)(c) != None)
       yield Or(vars(r)(c)(sudoku(r)(c).get - 1))
-    val allConstraints = onePerEntry ++ uniqueInColumns ++ uniqueInRows ++ uniqueInGrid ++ forcedEntries
-
-    var literalCounter = 0
-    var varToLiteral = Map[Formula, Int]()
-    def insert(v: Formula, pol: Boolean) = varToLiteral.get(v) match {
-      case Some(id) => new Literal(id, pol)
-      case None => {
-        varToLiteral += (v -> literalCounter)
-        literalCounter += 1
-        new Literal(literalCounter-1, pol)
-      }
-    }
-    def toCNF(cnstrs: Seq[Formula]) = {
-      cnstrs.map{
-        case Or(lits) => new Clause(lits.map{
-          case v@PropositionalVariable(name) => insert(v, true)
-          case Not(v@PropositionalVariable(name)) => insert(v, false)
-        })
-      }
-    }
-
-    regolic.sat.DPLL.isSat(toCNF(allConstraints).toList, literalCounter) match {
+    val allConstraints = onePerEntry ++ uniqueInColumns ++ uniqueInRows ++ uniqueInGrid1 ++ uniqueInGrid2 ++ forcedEntries
+    solve(And(allConstraints:_*)) match {
       case None => None
-      case Some(model) =>
-        Some(vars.map(row => row.map(vs => vs.indexWhere(v => model(varToLiteral(v))) + 1)))
+      case Some(model) => Some(vars.map(row => row.map(vs => vs.indexWhere(v => model(v)) + 1)))
     }
   }
 
